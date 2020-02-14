e9fa31d072d422f6ce42676550e51afd0738de4b
wulalago <dhr9936@gmail.com>
2020-02-14 10:08:47 -0500

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -15,7 +15,7 @@ class DiceLoss(nn.Module):
         tflat = target.contiguous().view(-1)
         intersection = (iflat * tflat).sum()
 
-        A_sum = torch.sum(tflat * iflat)
+        A_sum = torch.sum(iflat * iflat)
         B_sum = torch.sum(tflat * tflat)
 
         return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth))
